2a90e623d85657503a49b993379406a810f79420
MFF Design <51934155+MFFDesign@users.noreply.github.com>
2022-03-29 15:49:10 -0600

    Uso de EEPROM en Hormiga 877
    Procedimientos verificado y funcionando para las funciones EEPROMSave() y EEPROMRead()

---
+++ EEPROM/EEPROM.c
@@ -0,0 +1,34 @@
+#include "EEPROM.h"
+#include "Hormiga877.h"
+
+void EEPROMSave(char addr, char data)
+{
+    while(EECON1bits.WR);
+    EEADR = addr;
+    EEDATA = data;
+    EECON1bits.EEPGD = 0;
+    EECON1bits.WREN = 1;
+    INTCONbits.GIE = 0;
+    EECON2 = 0x55;
+    EECON2 = 0xAA;
+    EECON1bits.WR = 1;
+    INTCONbits.GIE = 1;
+    EECON1bits.WREN = 0;
+}
+
+char  EEPROMRead(char addr)
+{
+    while(EECON1bits.RD || EECON1bits.WR);
+    EEADR = addr;
+    EECON1bits.EEPGD = 0;
+    EECON1bits.RD = 1;
+    return EEDATA;
+}
+
+void SaveData(unsigned int Data, char InitAddress)
+{
+    char buffer = (Data >> 8) & 0xFF;
+    EEPROMSave(buffer,InitAddress);
+    buffer = Data & 0xFF;
+    EEPROMSave(buffer, InitAddress + 1);
+}
---
+++ EEPROM/EEPROM.h
@@ -0,0 +1,27 @@
+/* 
+ * File:   EEPROM.h
+ * Author: ToncheLAP
+ *
+ * Created on February 23, 2022, 2:43 PM
+ */
+
+#ifndef EEPROM_H
+#define	EEPROM_H
+
+void EEPROMSave(char addr, char data);
+char  EEPROMRead(char addr);
+void SaveData(unsigned int Data, char InitAddress);
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* EEPROM_H */
+
